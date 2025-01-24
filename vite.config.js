60432cb3df98d5c032154990dd876970db9cb446
Vladimir OVERWEEQ <130247406+OVERWEEQ@users.noreply.github.com>
2025-01-24 05:14:45 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        home: 'home.html',
+        category: 'category.html',
+        article: 'article.html',
+      },
+    },
+  },
+});
